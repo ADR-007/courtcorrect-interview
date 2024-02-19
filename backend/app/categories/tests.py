a1f86f6061e60eaac2f365990f4f30182a5030ef
Adrian Dankiv <adr-007@ukr.net>
2024-02-19 14:52:23 +0100

    test: add tests

--- backend/app/categories/tests.py
+++ backend/app/categories/tests.py
@@ -1,1 +1,25 @@
-# TODO: TBD
+from http import HTTPStatus
+
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.categories.models import Category
+
+URL = '/v1/categories'
+
+
+async def test_get_all(
+    db: AsyncSession,
+    client: AsyncClient,
+    category: Category,
+) -> None:
+    response = await client.get(URL)
+
+    assert response.status_code == HTTPStatus.OK
+
+    data = response.json()['items']
+    assert len(data) == 1
+    assert data[0] == {
+        'id': category.id,
+        'name': category.name,
+    }
--- backend/app/decisions/tests.py
+++ backend/app/decisions/tests.py
@@ -1,1 +1,23 @@
-# TODO: TBD
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.decisions.models import Decision
+
+URL = '/v1/decisions'
+
+
+async def test_get_all(
+    db: AsyncSession,
+    client: AsyncClient,
+    decision: Decision,
+) -> None:
+    response = await client.get(URL)
+
+    assert response.status_code == 200
+
+    data = response.json()['items']
+    assert len(data) == 1
+    assert data[0] == {
+        'id': decision.id,
+        'name': decision.name,
+    }
